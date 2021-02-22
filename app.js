a85a7f4f8c2f6913b312b5ff760d29cbe5d0ab19
mkp0 <shadymasum@gmail.com>
2021-02-22 20:52:05 +0530

    implementing-simple-cloudinary-upload page

---
+++ app.js
@@ -0,0 +1,25 @@
+const API_Base_URL = "https://api.cloudinary.com/v1_1/{put_yours_own}/upload";
+const PRESET = "{put_yours_own}";
+
+const file = document.getElementById("myFile");
+const submit = document.getElementById("submit");
+
+submit.addEventListener("click", (e) => {
+  e.preventDefault();
+  const myfile = file.files[0];
+  const fd = new FormData();
+  fd.append("file", myfile);
+  fd.append("upload_preset", PRESET);
+
+  fetch(clode_base_url, {
+    method: "POST",
+    body: fd,
+  })
+    .then((res) => res.json())
+    .then((res) => {
+      console.log(res);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
